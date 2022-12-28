7dcac785d023d3b85be080a1dd8144d3ddfffb23
smhrd <smhrd@DESKTOP-56Q4LGK>
2022-12-28 15:52:32 +0900

    2022-12-28 오승원 LikeCheckCon 누락

---
+++ FirstProject/src/main/java/com/ITssue/controller/LikeCheckCon.java
@@ -0,0 +1,63 @@
+package com.ITssue.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ITssue.dao.BoardMapper;
+import com.ITssue.dao.LikesMapper;
+import com.ITssue.entity.Board;
+import com.ITssue.entity.Likes;
+import com.google.gson.Gson;
+
+public class LikeCheckCon implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		int boardNo = Integer.parseInt(request.getParameter("board"));
+		String id = request.getParameter("id");
+		
+		Likes dto = new Likes();
+		
+		dto.setBoard_no(boardNo);
+		dto.setId(id);
+		
+		LikesMapper dao = new LikesMapper();
+		Likes check = dao.likeCheck(dto);
+		
+		int result = 0;
+		BoardMapper boardDao = new BoardMapper();
+		if(check != null) {
+			// 이미 추천을 한 경우
+			result = dao.likeDelete(dto);
+			boardDao.likeMinus(boardNo);
+		}else {
+			// 추천을 하지 않은 경우
+			result = dao.likeInsert(dto);
+			boardDao.likePlus(boardNo);
+		}
+		
+		
+		Board boardInfo = null;
+		if(result > 0) {
+			System.out.println("삭제 또는 추가 성공");
+			boardInfo = boardDao.boardInfo(boardNo);
+		}else {
+			System.out.println("완전실패");
+		}
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(boardInfo);
+		
+		response.getWriter().print(json);
+		
+		
+		return null;
+	}
+
+}
